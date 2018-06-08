a7efa833afe970c67ccc5b7395025d163d32e9df
Loserbirdman5566 <kobe24684@gmail.com>
2018-06-08 16:15:48 +0800

    Create new, create method, new Form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,23 @@ class TasksController < ApplicationController
   def index
    @tasks = Task.all
   end
+    # Create CRUD's create; new action
+  def new
+   @task = Task.new
+  end
+   
+    # Cteate CURD's create: create action
+   def create
+     @task = Task.new(task_params)
+     @task.save
+
+     redirect_to tasks_url
+   end
+
+   private 
+
+   def task_params
+     params.require(:task).permit(:name, :due_date, :note)
+   end
 
 end
